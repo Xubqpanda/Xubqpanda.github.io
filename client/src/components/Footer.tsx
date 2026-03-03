e81955142fb24544d9b9349d742fbfa4f1ba1d26
Buqiang Xu <Xubqpanda@gmail.com>
2026-03-03 18:45:10 +0800

    chore: update footer with Buqiang Xu's information

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -39,17 +39,17 @@ export default function Footer() {
     <footer className="bg-gray-900 text-gray-400 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm text-gray-500 font-light">
-          © {currentYear} Yibin (Leon) Liu. Site last updated {lastUpdated}
+          © {currentYear} Buqiang Xu. Site last updated {lastUpdated}
         </p>
         <p className="text-center text-sm text-gray-500 font-light mt-2">
           Feel free to fork this GitHub and develop your website!{' '}
           <a 
-            href="https://github.com/10-OASIS-01/10-OASIS-01.github.io" 
+            href="https://github.com/xubqpanda/xubqpanda.github.io" 
             target="_blank" 
             rel="noopener noreferrer"
             className="text-blue-400 hover:text-blue-300 transition-colors duration-200"
           >
-            @10-OASIS-01/10-OASIS-01.github.io
+            @xubqpanda/xubqpanda.github.io
           </a>
         </p>
         
